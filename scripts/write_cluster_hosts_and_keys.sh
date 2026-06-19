9d5fbc3a6087b8e0eceecfdb718145b5e9857a30
Bangchis <tienqldtdata@gmail.com>
2026-06-19 14:38:46 +0700

    Support per-node SSH users

--- scripts/write_cluster_hosts_and_keys.sh
+++ scripts/write_cluster_hosts_and_keys.sh
@@ -13,6 +13,9 @@ fi
 source "$env_file"
 
 CLUSTER_USER="${CLUSTER_USER:-$(id -un)}"
+MASTER_USER="${MASTER_USER:-$CLUSTER_USER}"
+NODE1_USER="${NODE1_USER:-$CLUSTER_USER}"
+NODE2_USER="${NODE2_USER:-$CLUSTER_USER}"
 
 for var in MASTER_IP NODE1_IP NODE2_IP MASTER_PUBLIC_KEY NODE1_PUBLIC_KEY NODE2_PUBLIC_KEY; do
   if [[ -z "${!var:-}" || "${!var}" == *REPLACE_WITH* ]]; then
@@ -49,6 +52,36 @@ done
 
 chmod 600 "$HOME/.ssh/authorized_keys"
 
+ssh_config="$HOME/.ssh/config"
+touch "$ssh_config"
+chmod 600 "$ssh_config"
+
+tmp_config="$(mktemp)"
+awk '
+  /# BEGIN parallel-macbook-cluster-setup/ {skip=1; next}
+  /# END parallel-macbook-cluster-setup/ {skip=0; next}
+  skip != 1 {print}
+' "$ssh_config" > "$tmp_config"
+
+{
+  echo "# BEGIN parallel-macbook-cluster-setup"
+  echo "Host master"
+  echo "  HostName $MASTER_IP"
+  echo "  User $MASTER_USER"
+  echo "  StrictHostKeyChecking accept-new"
+  echo "Host node1"
+  echo "  HostName $NODE1_IP"
+  echo "  User $NODE1_USER"
+  echo "  StrictHostKeyChecking accept-new"
+  echo "Host node2"
+  echo "  HostName $NODE2_IP"
+  echo "  User $NODE2_USER"
+  echo "  StrictHostKeyChecking accept-new"
+  echo "# END parallel-macbook-cluster-setup"
+} >> "$tmp_config"
+
+mv "$tmp_config" "$ssh_config"
+chmod 600 "$ssh_config"
+
 echo "HOSTS_AND_KEYS_READY=YES"
 getent hosts master node1 node2 || true
-
